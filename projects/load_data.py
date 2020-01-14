f16a35d645bee82be0b6bacea72ec3cbb9e93bab
Anna-Wasik <ania.wasik.19@gmail.com>
2020-01-14 16:27:31 +0100

    program do ładowania danych rozszerzony o program do wyświetlania ich

--- projects/load_data.py
+++ projects/load_data.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 from tqdm import tqdm
-
+import matplotlib.pyplot as plt
 
 def load_data(dir_path, img_size=(100,100)):
     """
@@ -37,4 +37,25 @@ X_test, y_test, _ = load_data(TEST_DIR, IMG_SIZE)
 X_val, y_val, _ = load_data(VAL_DIR, IMG_SIZE)
 
         
+def plot_samples(X, y, labels_dict, n=50):
+    """
+    Creates a gridplot for desired number of images (n) from the specified set
+    """
+    for index in range(len(labels_dict)):
+        imgs = X[np.argwhere(y == index)][:n]
+        j = 10
+        i = int(n/j)
+
+        plt.figure(figsize=(15,6))
+        c = 1
+        for img in imgs:
+            plt.subplot(i,j,c)
+            plt.imshow(img[0])
+
+            plt.xticks([])
+            plt.yticks([])
+            c += 1
+        plt.suptitle('Tumor: {}'.format(labels_dict[index]))
+        plt.show()
         
+plot_samples(X_train, y_train, labels, 30)     
