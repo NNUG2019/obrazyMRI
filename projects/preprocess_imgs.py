48a5ce588a0c61d470b148b838aa9d4b0c0408b5
Anna-Wasik <ania.wasik.19@gmail.com>
2020-01-14 18:38:42 +0100

    programy do katalogu projects oraz nowe dane uzyskane z notebooka

---
+++ projects/preprocess_imgs.py
@@ -0,0 +1,16 @@
+import cv2
+
+
+def preprocess_imgs(set_name, img_size):
+    """
+    Resize and apply VGG-15 preprocessing
+    """
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(
+            img,
+            dsize=img_size,
+            interpolation=cv2.INTER_CUBIC
+        )
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
